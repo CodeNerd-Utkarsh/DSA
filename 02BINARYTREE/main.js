a9194e2f4f40b73668d6fb0841d6c0a1f2545a89
CodeNerd-Utkarsh <hbase_groups_official@aol.com>
2024-02-23 14:12:02 +0530

    created a Breadth-first approach using a queue

--- 02BINARYTREE/main.js
+++ 02BINARYTREE/main.js
@@ -26,7 +26,28 @@ b.leftChild = d
 b.rightChild = e
 c.leftChild = f
 
-//  TODO: Depth first approach => [a, b, d, e, c, f]=>  uses a stack
+//  TODO: Breadth-first approach => [a, b, d, e, c, f]=>  uses a queue
+//  * push a node in the queue
+//  * pop the front, check if it's having (left or right) children
+//  * insert the children nodes in the queue
+//  * repeat till queue is empty
+
+function BFA(head) {
+    let queue = [head]
+    while (queue.length > 0) {
+        let pointer = queue.shift()
+        console.log(pointer.val)
+        if (pointer.leftChild) {
+            queue.push(pointer.leftChild)
+        }
+        if (pointer.rightChild) {
+            queue.push(pointer.rightChild)
+        }
+    }
+}
+BFA(a)
+
+//  TODO: Depth-first approach => [a, b, c, d, e, f]=>  uses a queue
 //  * push a node in the stack
 //  * pop the top, check if it's having (left or right) children
 //  * insert the children nodes at top of the stack
@@ -45,4 +66,4 @@ function DFA(head) {
         }
     }
 }
-DFA(a)
+// DFA(a)
--- 02BINARYTREE/main.py
+++ 02BINARYTREE/main.py
@@ -27,7 +27,25 @@ b.leftChild = d
 b.rightChild = e
 c.leftChild = f
 
-#  TODO: Depth first approach => [a, b, d, e, c, f]=>  uses a stack
+# TODO: Breadth-first approach => [a, b, c, d, e, f]=>  uses a queue
+# * push a node in the queue
+# * pop the front, check if it's having (left or right) children
+# * insert the children nodes in the queue
+# * repeat till the queue is empty
+
+def BFA(head):
+    queue=[head]
+    while(len(queue)>0):
+        pointer= queue.pop()
+        print(pointer.val)
+        if(pointer.leftChild):
+            queue.insert(0,(pointer.leftChild))
+        if(pointer.rightChild):
+            queue.insert(0,(pointer.rightChild))    
+    
+BFA(a)
+
+#  TODO: Depth-first approach => [a, b, d, e, c, f]=>  uses a stack
 #  * push a node in the stack
 #  * pop the top, check if it's having (left or right) children
 #  * insert the children nodes at top of the stack
@@ -43,4 +61,4 @@ def DFA(head):
         if(pointer.leftChild):
             stack.append(pointer.leftChild)    
     
-DFA(a)
+# DFA(a)
